b3d4b38ff370c557d9ddea00c2c0a2c9a333ec33
Htoo Maung Thait <hmthait@blueplanetmm.com>
2024-02-26 11:12:11 +0630

    first commit

---
+++ src/main/java/com/springdemo/firstDemo/HelloController.java
@@ -0,0 +1,19 @@
+package com.springdemo.firstDemo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String index(){
+        return "Greeting from Spring Boot!";
+    }
+
+    @GetMapping("/show-text")
+    public String showText(){
+        return "Hello this testing route showing a text!";
+    }
+
+}
---
+++ src/main/java/com/springdemo/firstDemo/restservice/Greeting.java
@@ -0,0 +1,2 @@
+package com.springdemo.firstDemo.restservice;
+public record Greeting(long id, String content) { }
